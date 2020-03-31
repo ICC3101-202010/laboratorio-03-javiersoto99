e1ea913bb6e129961e0586b853ecb594ef81f3cb
Javier Soto <jisoto2@miuandes.cl>
2020-03-31 17:28:44 -0300

    first commit

---
+++ lab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
